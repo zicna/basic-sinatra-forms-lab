b14c301b0bbe19019794b81b2e6dc94da0dd7b0a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-14 23:55:32 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        
+        erb :newteam
+    end
+
+    post '/team' do
+       @team = params[:name]
+       @coach = params[:coach]
+       @point_guard = params[:pg]
+       @shooting_guard = params[:sg]
+       @power_forward = params[:pf]
+       @center = params[:c]
+
+        erb :team
+    end
 
 
 end
